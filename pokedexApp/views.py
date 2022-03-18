081385481aeafa1cb60506eb85831a0fe7fef68c
Ivan Y <ivany@subetuweb.site>
2022-03-18 01:07:29 -0600

    first approch

--- pokedexApp/views.py
+++ pokedexApp/views.py
@@ -1,5 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
+import logging
 
 def index(request):
-    return render(request, 'pokedexApp/index.html')
+    response = requests.get('https://pokeapi.co/api/v2/pokemon/bulbasaur')
+    dataJson = response.json()
+    namePokemon = dataJson["forms"][0]["name"].capitalize()
+    idPokemon = dataJson["id"]    
+    heightPokemon = float(dataJson["height"]) / 10  
+    weightPokemon = float(dataJson["weight"]) / 10
+    imageUrlPokemon = dataJson["sprites"]["other"]["home"]["front_default"]
+    typesPokemon = []
+    statsPokemon = []
+
+    # checar si nada mas tiene un tipo
+    for i in dataJson["types"]:
+        #logging.debug("Log message goes here.")
+        typesPokemon.append(i["type"]["name"])
+    
+    for i in dataJson["stats"]:
+        statsPokemon.append([i["stat"]["name"], i["base_stat"]])
+       
+
+    return render(request, 'pokedexApp/index.html', {
+        "dataJson": dataJson, 
+        "namePokemon": namePokemon, 
+        "idPokemon": idPokemon,
+        "typesPokemon": typesPokemon,
+        "statsPokemon": statsPokemon,
+        "heightPokemon": heightPokemon,
+        "weightPokemon": weightPokemon,
+        "imageUrlPokemon": imageUrlPokemon
+        })
