056e75c94e29dddd70f5f02435bda353077d1e5c
J-ATJ <jyoruge6798@gmail.com>
2022-03-17 23:19:05 -0600

    Pruebas1: form

--- pokedexApp/views.py
+++ pokedexApp/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django import forms
+
+class pokechosencl(forms.Form):
+    pokechosen = forms.CharField(label = "Ingresa un Pokemon")
+
+ditto = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/132.png"
 
 def index(request):
-    return render(request, 'pokedexApp/index.html')
+    pkimg = "none"
+    if request.method == 'POST':
+        pkFormUser = pokechosencl(request.POST)
+        if pkFormUser.is_valid():
+            pkFormUserCleaned = pkFormUser.cleaned_data['pokechosen']
+            pkFormUserCleaned.lower()
+        if pkFormUserCleaned == 'ditto':
+            pkimg = ditto
+
+        else:
+            return render(request, 'pokedexApp/index.html', {"pkc": pokechosencl(), "pkimg": pkimg}) 
+    return render(request, 'pokedexApp/index.html', {"pkc": pokechosencl(), "pkimg": pkimg})
