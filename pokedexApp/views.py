89af838c5b0c9da51a7a21782c7d23eeb568ac99
Ivan Y <ivany@subetuweb.site>
2022-03-20 01:01:12 -0600

    Some errors and bootstrap

--- pokedexApp/views.py
+++ pokedexApp/views.py
@@ -1,22 +1,31 @@
+from logging import PlaceHolder
 from django.shortcuts import render
 from django.http import HttpResponse
 from django import forms
 import requests
 
 class PokeChosen(forms.Form):
-    varPokeChosen = forms.CharField(label= "Chose: ")
+    varPokeChosen = forms.CharField(
+        label='',
+        widget=forms.TextInput(attrs={'placeholder': 'Search'})) #Poke name or ID
 
 def index(request):
-
+    flag = False
     url = "https://pokeapi.co/api/v2/pokemon/1"
-
+    
     if request.method == "POST":
-        inputUser = PokeChosen(request.POST)
+        inputUser = PokeChosen(request.POST)        
         if inputUser.is_valid():
             inputUserClened = inputUser.cleaned_data["varPokeChosen"].lower()
             url = "https://pokeapi.co/api/v2/pokemon/"+inputUserClened
     
     response = requests.get(url)
+    try:
+        response.raise_for_status()
+    except requests.exceptions.HTTPError:
+        flag = True
+        response = requests.get("https://pokeapi.co/api/v2/pokemon/1")        
+    
     dataJson = response.json()
     namePokemon = dataJson["forms"][0]["name"].capitalize()
     idPokemon = dataJson["id"]    
@@ -31,9 +40,10 @@ def index(request):
     
     for i in dataJson["stats"]:
         statsPokemon.append([i["stat"]["name"], i["base_stat"]])
-       
+    
 
     return render(request, 'pokedexApp/index.html', {
+        "flag": flag,
         "dataJson": dataJson,
         "namePokemon": namePokemon,
         "idPokemon": idPokemon,
